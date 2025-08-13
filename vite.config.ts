2c6e7505c0d8816d84a9e8f86df1ee64ccba843e
Vitaliy <vitaliysamborsky2002@gmail.com>
2025-08-13 15:16:08 +0300

    chore(assets-config): add images and fonts, rework base project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "/test-work-abz-agency/",
+	server: {
+		port: 3000,
+	},
+	build: {
+		outDir: "dist",
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true,
+				additionalData: `@import "/src/scss/global/_vars.scss";
+												 @import "/src/scss/global/_mixins.scss";`,
+			},
+		},
+	},
+});
